58042543d6d4e0f5f0c9883d25d73d0384b1ce45
Hsuan19971005 <g19971005@gmail.com>
2023-06-30 22:55:25 +0800

    Refine Q49 with group_by

--- LeetCode/GroupAnagrams.rb
+++ LeetCode/GroupAnagrams.rb
@@ -4,17 +4,20 @@
 
 # @param {String[]} strs
 # @return {String[][]}
+# def group_anagrams(strs)
+#   hash = {}
+#   strs.each do |str|
+#     sorted_str = str.split("").sort.join
+#     if hash[sorted_str] == nil
+#       hash[sorted_str] = [str]
+#     else
+#       hash[sorted_str] << str
+#     end
+#   end
+#   hash.values
+# end
 def group_anagrams(strs)
-  hash = {}
-  strs.each do |str|
-    sorted_str = str.split("").sort.join
-    if hash[sorted_str] == nil
-      hash[sorted_str] = [str]
-    else
-      hash[sorted_str] << str
-    end
-  end
-  hash.values
+  strs.group_by{|str| str.chars.sort.join}.values
 end
 
 p group_anagrams(["eat","tea","tan","ate","nat","bat"]) # [["bat"],["nat","tan"],["ate","eat","tea"]]
