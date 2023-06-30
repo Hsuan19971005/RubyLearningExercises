acac29afcda176b496c02f9068d7a6e4c23807d9
Hsuan19971005 <g19971005@gmail.com>
2023-06-30 22:44:14 +0800

    Solve Q49 Group Anagrams

---
+++ LeetCode/GroupAnagrams.rb
@@ -0,0 +1,22 @@
+# 49. Group Anagrams
+# Medium
+# Related Topics: Array, Hash Table, String, Sorting
+
+# @param {String[]} strs
+# @return {String[][]}
+def group_anagrams(strs)
+  hash = {}
+  strs.each do |str|
+    sorted_str = str.split("").sort.join
+    if hash[sorted_str] == nil
+      hash[sorted_str] = [str]
+    else
+      hash[sorted_str] << str
+    end
+  end
+  hash.values
+end
+
+p group_anagrams(["eat","tea","tan","ate","nat","bat"]) # [["bat"],["nat","tan"],["ate","eat","tea"]]
+p group_anagrams([""]) # [[""]]
+p group_anagrams(["a"]) # [["a"]]
