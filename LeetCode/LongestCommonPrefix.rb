4a1087e0e8604fde1121ab38ba8d1750784992cc
Hsuan19971005 <g19971005@gmail.com>
2023-07-01 21:19:53 +0800

    Refine Q14 with sort and O(n log n)

--- LeetCode/LongestCommonPrefix.rb
+++ LeetCode/LongestCommonPrefix.rb
@@ -4,23 +4,33 @@
 
 # @param {String[]} strs
 # @return {String}
-def longest_common_prefix(strs)
-  result = ""
-  strs.each.with_index do |str, index|
-    if index == 0 
-      result = str
-      next
-    end
+# def longest_common_prefix(strs)
+#   result = ""
+#   strs.each.with_index do |str, index|
+#     if index == 0 
+#       result = str
+#       next
+#     end
     
-    result.chars.each.with_index do |e, i|
-      if e != str[i]
-        result = str.slice(0, i)
-        break
-      end
-    end
+#     result.chars.each.with_index do |e, i|
+#       if e != str[i]
+#         result = str.slice(0, i)
+#         break
+#       end
+#     end
+#   end
+#   result
+# end
+def longest_common_prefix(strs)
+  strs.sort!
+  first_str = strs.first
+  last_str = strs.last
+  first_str.length.times.with_index do |str, index|
+    return first_str.slice(0, index) if first_str[index] != last_str[index]
   end
-  result
+  first_str
 end
+
 
 p longest_common_prefix(["flower","flow","flight"]) # "fl "
 p longest_common_prefix(["dog","racecar","car"]) # ""
